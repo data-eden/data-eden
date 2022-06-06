501cea74895527915e7882658e1207f4ac5d54e6
Gabriel J. Csapo <gabecsapo@gmail.com>
2022-06-06 14:36:56 -0700

    chore: adds docs based on review feedback

--- packages/network/src/fetch.ts
+++ packages/network/src/fetch.ts
@@ -5,20 +5,32 @@ export type Fetch = typeof fetch;
 export type NormalizedFetch = (request: Request) => Promise<Response>;
 
 export type Middleware = (
+  /**
+   * Request object to be manipulated by middleware
+   */
   request: Request,
+  /**
+   * localized fetch used when all middlewares are done with manipulating Request
+   */
   next: NormalizedFetch
 ) => Promise<Response>;
 
 export interface BuildFetchOptions {
-  // Whether to force earlier built fetches to error making the most recent //
-  // invokation the authoritive fetch. You will typically only want to set this to
-  // false for testing. Defaults to true.
+  /**
+   * Whether to force earlier built fetches to error making the most recent //
+   * invokation the authoritive fetch. You will typically only want to set this to
+   * false for testing. Defaults to true.
+   */
   disablePrior?: boolean;
-  // What message to throw if a user tries to invoke a disabled fetch. Useful
-  // to help users know where to import fetch from rather than build it //
-  // themselves.
+  /**
+   * What message to throw if a user tries to invoke a disabled fetch. Useful
+   * to help users know where to import fetch from rather than build it //
+   * themselves.
+   */
   disableMessage?: string;
-  // override the default cross-fetch implementation
+  /**
+   * override the default cross-fetch implementation
+   */
   fetch: Fetch;
 }
 
@@ -31,6 +43,12 @@ function combine(
   };
 }
 
+/**
+ * 
+ * @param middlewares {Middleware[]} array of middlewares
+ * @param options {BuildFetchOptions=} optional options for overriding buildFetch configuration
+ * @returns Fetch
+ */
 export function buildFetch(
   middlewares: Middleware[],
   options?: BuildFetchOptions
