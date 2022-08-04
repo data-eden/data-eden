c019ff4c99dcc17e1194ac5ded1b3fa753d60d35
Gabriel J. Csapo <gabecsapo@gmail.com>
2022-08-04 15:50:43 -0700

    chore: makes changes to be able to consume as an esm package

--- packages/network/src/index.ts
+++ packages/network/src/index.ts
@@ -1,3 +1,3 @@
-export { buildFetch } from './fetch';
+export { buildFetch } from './fetch.js';
 
-export type { Middleware, NormalizedFetch } from './fetch';
+export type { Middleware, NormalizedFetch } from './fetch.js';
