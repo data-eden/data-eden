061dc5790c9860ac35949f162ad899ad4ce59d40
Gabriel Csapo <gabecsapo@gmail.com>
2022-08-11 01:22:22 -0700

    feat(docs): create documentation scaffolding and adds autogenerating API docs

---
+++ website/docusaurus.config.js
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const path = require('path');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'data-eden',
+  tagline: '',
+  url: 'https://data-eden.github.io',
+  baseUrl: '/data-eden/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'data-eden',
+  projectName: 'data-eden',
+  plugins: [
+    [
+      require.resolve('docusaurus-plugin-search-local'),
+      {
+        highlightSearchTermsOnTargetPage: true,
+        docsRouteBasePath: ['docs', 'api'],
+      },
+    ],
+    [
+      'docusaurus-plugin-typedoc-api',
+      {
+        projectRoot: path.join(__dirname, '..'),
+        packages: ['packages/network', 'packages/cache', 'packages/athena'],
+      },
+    ],
+  ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          editUrl: 'https://github.com/data-eden/data-eden/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/data-eden/data-eden/edit/main/website/blog/',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'data-eden',
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            to: 'api/network',
+            label: 'API',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/data-eden/data-eden',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/data-eden/data-eden',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} data-eden, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
