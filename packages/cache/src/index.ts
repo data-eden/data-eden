777f30af9a0eece310b631c7813d5136975fcb59
Shuba Krishnan <shkrishnan@linkedin.com>
2023-04-03 00:07:23 +0100

    Decouple Public API and impl details

--- packages/cache/src/index.ts
+++ packages/cache/src/index.ts
@@ -1,1 +1,18 @@
 export { buildCache } from './cache.js';
+export type {
+  Cache,
+  CacheTransaction,
+  LiveCacheTransaction,
+  CommittingTransaction,
+  CacheEntry,
+  CacheEntryState,
+  CacheKeyValue,
+  EntityMergeStrategy,
+  RevisionMergeStrategy,
+  CachedEntityRevision,
+  ExpirationPolicy,
+  CacheOptions,
+  DefaultRegistry,
+  CacheDebugAPIs,
+  CacheTransactionDebugAPIs
+} from './api.js'
