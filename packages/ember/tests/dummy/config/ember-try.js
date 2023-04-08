98b8dfae6a17dc786a443375fe00d38bcd860ee4
Shuba Krishnan <shkrishnan@linkedin.com>
2023-04-08 16:40:27 +0100

    fix ember-beta

--- packages/ember/tests/dummy/config/ember-try.js
+++ packages/ember/tests/dummy/config/ember-try.js
@@ -37,6 +37,8 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('beta'),
+            'ember-resolver': '10.0.0',
+            '@ember/string': '3.0.1',
           },
         },
       },
