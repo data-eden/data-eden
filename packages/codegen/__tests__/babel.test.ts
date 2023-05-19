5f3b8f1a174d96df5c909e69f905f932d5b21ac3
Gabriel J. Csapo <gabecsapo@gmail.com>
2023-05-18 18:32:34 -0700

    [codegen] remove @data-eden/codegen/gql when running babel transform

--- packages/codegen/__tests__/babel.test.ts
+++ packages/codegen/__tests__/babel.test.ts
@@ -45,7 +45,6 @@ export const FindUserDocument = {"__meta__":{"queryId":"d17490e4b9ac1f7c227df3da
     expect(result && result.code).toMatchInlineSnapshot(`
       "import { FindUserDocument } from \\"./__generated/User.graphql.ts\\";
       import { UserFieldsFragmentDoc } from \\"./__generated/User.graphql.ts\\";
-      import { gql } from '@data-eden/codegen/gql';
       const userFieldsFragment = UserFieldsFragmentDoc;
       const findUserQuery = FindUserDocument;"
     `);
@@ -70,7 +69,6 @@ export const FindUserDocument = {"__meta__":{"queryId":"d17490e4b9ac1f7c227df3da
 
     expect(result && result.code).toMatchInlineSnapshot(`
       "import { FindUserDocument } from \\"./__generated/User.graphql.ts\\";
-      import { gql } from '@data-eden/codegen/gql';
       import { userFieldsFragment } from './UserView.tsx';
       const findUserQuery = FindUserDocument;"
     `);
