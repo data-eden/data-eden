3f1fb1c35393456cdeec9984a334f3829e104de7
Gabriel J. Csapo <gabecsapo@gmail.com>
2023-06-28 15:23:30 -0700

    wip

--- packages/new-athena/__tests__/signal-cache.test.ts
+++ packages/new-athena/__tests__/signal-cache.test.ts
@@ -61,4 +61,336 @@ describe('SignalCache', () => {
       expect(key).toEqual(null);
     });
   });
+
+  describe('#parse', () => {
+    beforeEach(() => {
+      cache = new SignalCache();
+    });
+
+    test('blah', () => {
+      const document = {
+        __typename: 'Query',
+        foo: {
+          __typename: 'Foo',
+          id: '1234',
+        },
+      };
+
+      const result = cache.parse(document.__typename, document);
+
+      expect(result.length).toEqual(2);
+      expect(result).toMatchInlineSnapshot(`
+        [
+          {
+            "entity": {
+              "__typename": "Foo",
+              "id": "1234",
+            },
+            "parent": {
+              "__typename": "Query",
+              "foo": {
+                "__typename": "Foo",
+                "id": "1234",
+              },
+            },
+            "prop": "foo",
+          },
+          {
+            "entity": {
+              "__typename": "Query",
+              "foo": {
+                "__typename": "Foo",
+                "id": "1234",
+              },
+            },
+            "parent": null,
+            "prop": "Query",
+          },
+        ]
+      `);
+    });
+
+    test('document with multiple levels', () => {
+      const document = {
+        __typename: 'Query',
+        person: {
+          id: '1',
+          name: 'foo',
+          __typename: 'Person',
+          pets: [
+            {
+              id: '1',
+              name: 'Hitch',
+              owner: {
+                id: '1',
+                name: 'foo',
+                __typename: 'Person',
+              },
+              __typename: 'Pet',
+            },
+            {
+              id: '2',
+              name: 'Dre',
+              owner: {
+                id: '1',
+                name: 'foo',
+                __typename: 'Person',
+              },
+              __typename: 'Pet',
+            },
+          ],
+        },
+      };
+
+      const result = cache.parse(document.__typename, document);
+
+      expect(result.length).toEqual(6);
+      expect(result).toMatchInlineSnapshot(`
+        [
+          {
+            "entity": {
+              "__typename": "Person",
+              "id": "1",
+              "name": "foo",
+            },
+            "parent": {
+              "__typename": "Pet",
+              "id": "2",
+              "name": "Dre",
+              "owner": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+              },
+            },
+            "prop": "owner",
+          },
+          {
+            "entity": {
+              "__typename": "Pet",
+              "id": "2",
+              "name": "Dre",
+              "owner": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+              },
+            },
+            "parent": {
+              "__typename": "Person",
+              "id": "1",
+              "name": "foo",
+              "pets": [
+                {
+                  "__typename": "Pet",
+                  "id": "1",
+                  "name": "Hitch",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+                {
+                  "__typename": "Pet",
+                  "id": "2",
+                  "name": "Dre",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+              ],
+            },
+            "prop": [
+              "pets",
+              "1",
+            ],
+          },
+          {
+            "entity": {
+              "__typename": "Person",
+              "id": "1",
+              "name": "foo",
+            },
+            "parent": {
+              "__typename": "Pet",
+              "id": "1",
+              "name": "Hitch",
+              "owner": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+              },
+            },
+            "prop": "owner",
+          },
+          {
+            "entity": {
+              "__typename": "Pet",
+              "id": "1",
+              "name": "Hitch",
+              "owner": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+              },
+            },
+            "parent": {
+              "__typename": "Person",
+              "id": "1",
+              "name": "foo",
+              "pets": [
+                {
+                  "__typename": "Pet",
+                  "id": "1",
+                  "name": "Hitch",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+                {
+                  "__typename": "Pet",
+                  "id": "2",
+                  "name": "Dre",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+              ],
+            },
+            "prop": [
+              "pets",
+              "0",
+            ],
+          },
+          {
+            "entity": {
+              "__typename": "Person",
+              "id": "1",
+              "name": "foo",
+              "pets": [
+                {
+                  "__typename": "Pet",
+                  "id": "1",
+                  "name": "Hitch",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+                {
+                  "__typename": "Pet",
+                  "id": "2",
+                  "name": "Dre",
+                  "owner": {
+                    "__typename": "Person",
+                    "id": "1",
+                    "name": "foo",
+                  },
+                },
+              ],
+            },
+            "parent": {
+              "__typename": "Query",
+              "person": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+                "pets": [
+                  {
+                    "__typename": "Pet",
+                    "id": "1",
+                    "name": "Hitch",
+                    "owner": {
+                      "__typename": "Person",
+                      "id": "1",
+                      "name": "foo",
+                    },
+                  },
+                  {
+                    "__typename": "Pet",
+                    "id": "2",
+                    "name": "Dre",
+                    "owner": {
+                      "__typename": "Person",
+                      "id": "1",
+                      "name": "foo",
+                    },
+                  },
+                ],
+              },
+            },
+            "prop": "person",
+          },
+          {
+            "entity": {
+              "__typename": "Query",
+              "person": {
+                "__typename": "Person",
+                "id": "1",
+                "name": "foo",
+                "pets": [
+                  {
+                    "__typename": "Pet",
+                    "id": "1",
+                    "name": "Hitch",
+                    "owner": {
+                      "__typename": "Person",
+                      "id": "1",
+                      "name": "foo",
+                    },
+                  },
+                  {
+                    "__typename": "Pet",
+                    "id": "2",
+                    "name": "Dre",
+                    "owner": {
+                      "__typename": "Person",
+                      "id": "1",
+                      "name": "foo",
+                    },
+                  },
+                ],
+              },
+            },
+            "parent": null,
+            "prop": "Query",
+          },
+        ]
+      `);
+    });
+
+    test('should account for multiple levels of unmanaged entity', () => {
+      const document = {
+        foo: {
+          id: '1',
+          __typename: 'Foo',
+          bar: {
+            baz: {
+              blah: 'blah',
+              owner: {
+                id: '1',
+                name: 'bob',
+                __typename: 'Owner',
+              },
+              __typename: 'Baz',
+            },
+            __typename: 'Bar',
+          },
+        },
+        __typename: 'Query',
+      };
+
+      const result = cache.parse(document.__typename, document);
+
+      console.log(result);
+    });
+  });
 });
--- packages/new-athena/src/index.ts
+++ packages/new-athena/src/index.ts
@@ -1,7 +1,11 @@
 import { type DocumentNode, type GraphQLError } from 'graphql';
 import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import type { Cache } from '@data-eden/cache';
-import { DefaultRegistry, buildCache } from '@data-eden/cache';
+import { type DefaultRegistry, buildCache } from '@data-eden/cache';
+import { set } from 'lodash-es';
+import { traverse } from './traverse.js';
+import type { Data, DataField, Entity } from './index.js';
+import type { Entries } from 'type-fest';
 
 export type Primitive =
   | null
@@ -15,6 +19,29 @@ export type Primitive =
 export type DefaultVariables = Record<string, object | Primitive> | undefined;
 export type DefaultRecord = Record<string, object>;
 
+interface LinkNode {
+  __link: string;
+}
+
+export function createLinkNode(key: string): LinkNode {
+  return {
+    __link: key,
+  };
+}
+
+export function isOperation(operation: unknown): operation is Operation {
+  return (
+    typeof operation === 'object' &&
+    operation !== null &&
+    'type' in operation &&
+    'name' in operation &&
+    'queryId' in operation
+  );
+}
+
+export type CacheKey = string;
+export type PropertyPath = string | number | Array<string | number>;
+
 export interface ClientError {
   graphql?: Array<string | GraphQLError>;
   network?: unknown;
@@ -23,6 +50,17 @@ export interface ClientError {
 export interface OperationResult {
   data?: Data;
   error?: ClientError;
+}
+
+export interface ParsedEntity {
+  entity: Entity;
+  parent: Entity | null;
+  prop: string | number | Array<string | number>;
+  id: string;
+}
+
+export function isEntity(obj: unknown): obj is Entity {
+  return typeof obj === 'object' && obj !== null && '__typename' in obj;
 }
 
 interface Operation<
@@ -47,40 +85,6 @@ interface Operation<
   result?: OperationResult;
 }
 
-/**
- * raw JSON blob -> normalize -> [ record, links ] -> resolve -> signal blob -> UI
- */
-
-/**
- * resolvers: {
- *  Query: {
- *    comments: pagination()
- *  }
- * }
- */
-
-// export const CreateOnePetDocument = {
-//   __meta__: {
-//     queryId: '2418fa9c2c9325b1484fd9c96c06a51871c2ca41893ddf3c57c318ae071fe9ec', } }
-
-// {Query: {
-//   comments: (op: Operation, oldValue: any, newValue: any) {
-//     if(op.queryId === CreateOnePetDocument.__meta__.queryId) {
-
-//     }
-//   }
-// }
-
-// export type CreateOnePetMutation = {
-//   __typename: 'Mutation';
-//   createPet: { __typename: 'Pet'; id: string; name: string };
-// };
-
-// 12345 -> query getComments() {
-//   id
-//   message
-// }
-
 export interface BaseFields {
   id?: string;
   __typename: string;
@@ -96,7 +100,7 @@ interface DataFields {
 
 export type Data = BaseFields & DataFields;
 
-export type Entity = null | Data;
+export type Entity = Data;
 
 export type KeyGetter = (data: Data) => string | null;
 
@@ -136,23 +140,149 @@ export class SignalCache {
 
     const tx = await this.cache.beginTransaction();
 
-    const key = this.getKey(result.data);
+    // This object maps "root" entities from a graphql docment to the key used to store them in the
+    // cache. We will later use this mapping to reconstitute all of the entities and construct
+    // the result to pass back to the caller
+    const roots = new Map<PropertyPath, CacheKey>();
+    const parsedEntities = this.parse(result.data.__typename, result.data);
 
-    if (!key) return null;
+    for (const { parent, prop, entity } of parsedEntities) {
+      let key = this.getKey(entity);
 
-    await tx.merge(key, result);
+      // replace the entity object with the key we're using to store it in the cache so that we can
+      // later replace the key with the reactive entity
+      // e.g. { pet: { id: 1, name: hitch }} -> { pet: 'pet:1' }
+      if (parent && prop) {
+        set(parent, prop, createLinkNode(key));
+      }
 
-    const entities =
+      if (!parent) {
+        roots.set(prop, key);
+      }
+
+      await tx.merge(key, entity);
+    }
 
     await tx.commit();
 
-    for await (let entry of tx.localEntries()) {
-      const [key, entity] = entry;
-      // Entity can also be string | number so we need to make sure it's actually an object here
-      if (isEntity(entity)) {
-        signalCache.storeEntity(key, entity, !!tx.context.fetchMore); //op.type
+    // for await (let entry of tx.localEntries()) {
+    //   const [key, entity] = entry;
+    //   // Entity can also be string | number so we need to make sure it's actually an object here
+    //   if (isEntity(entity)) {
+    //     signalCache.storeEntity(key, entity, !!tx.context.fetchMore); //op.type
+    //   }
+    // }
+  }
+
+  // DFS through each top-level element in document.data because it's possible that a gql operation
+  // can return sibling entities and we need to treat each of them as root entities for the sake
+  // of finding child entities
+  parse(
+    prop: string | number | Array<string | number>,
+    entry: DataField
+  ): Array<ParsedEntity> {
+    const result: Array<ParsedEntity> = [];
+
+    // This tracks the property we're in if we are currently traversing through an array
+    let currentArrayProp: string | null;
+    let ArrayMetadata = new WeakMap<
+      Array<Entity>,
+      { key: string; parent: Entity }
+    >();
+
+    let root: Entity | undefined = undefined;
+
+    // This is the root level operation
+    if (isEntity(entry)) {
+      const key = this.getKey(entry);
+      if (!key) {
+        throw new Error(
+          `@data-eden/athena: We couldn't find a key for ${entry.__typename} and could not generate a synthetic one since this is a top level object. Provide a key generator in \`keys\` for ${entry.__typename}.`
+        );
       }
+
+      root = entry;
+      result.push({
+        id: key,
+        entity: entry,
+        parent: null,
+        prop: prop,
+      });
     }
+
+    traverse(entry, (key, value, parent) => {
+      let parentEntity: Entity | null = null;
+
+      if (isEntity(value)) {
+        if (Array.isArray(parent)) {
+          // if we're here, it means we're in a hasMany relationship and need to track which property
+          // got us here so we can refer back to the property key when linking entities later,
+          // e.g. { pets: [ { name: 'hitch', id: 1 }]} will have a prop value of `pets[0]` so we know
+          // how to get to it from the parent
+          const meta = ArrayMetadata.get(parent as Array<Entity>) || null;
+          if (meta) {
+            currentArrayProp = meta.key;
+            parentEntity = meta.parent;
+          }
+        } else {
+          if (isEntity(parent)) {
+            parentEntity = parent;
+          }
+          currentArrayProp = null;
+        }
+
+        if (result.length === 0) {
+          const key = this.getKey(value);
+
+          if (!key) {
+            throw new Error(
+              `@data-eden/athena: We couldn't find a key for ${value.__typename} and could not generate a synthetic one since this is a top level object. Provide a key generator in \`keys\` for ${value.__typename}.`
+            );
+          }
+
+          // if the result array is empty, then this is the first entity we've found, and is therefore
+          // the root entity, so we special-case it and set the parent and prop
+          result.push({
+            id: key,
+            entity: value,
+            parent: null,
+            prop: key,
+          });
+        } else {
+          const prop = currentArrayProp ? [currentArrayProp, key] : key;
+
+          result.push({
+            id: this.getKey(value) ?? `${parentEntity.id}:${prop}`,
+            entity: value,
+            parent: parentEntity,
+            prop,
+          });
+        }
+
+        return true;
+      }
+
+      if (Array.isArray(value)) {
+        if (!Array.isArray(parent) && isEntity(parent)) {
+          // If we've arrived here, we know that we're traversing through an array of entities
+          // because the parent is an entity and the current value is an array
+          ArrayMetadata.set(value as Array<Entity>, {
+            parent,
+            key,
+          });
+        }
+        return true;
+      }
+
+      // If we haven't found the root yet, just keep digging
+      if (root === undefined) {
+        return true;
+      }
+
+      return false;
+    });
+
+    return result.reverse();
   }
 
   getKey(entity: Entity): null | string {
@@ -175,13 +305,79 @@ export class SignalCache {
     } else if (entity.id) {
       return entity.id;
     } else {
+      console.warn(
+        `@data-eden/athena: no key derived for ${typename}. If this is intentional please provide a key generator in \`keys\` for ${typename} that always returns null.`
+      );
+
       return null;
     }
   }
 
-    async processEntities<Data extends { [key: string]: any }>(
+  computeCacheKey(entity: Entity): string;
+  computeCacheKey(operation: Operation): string;
+  computeCacheKey(entityOrOperation: Entity | Operation): string {
+    if (isOperation(entityOrOperation)) {
+      const stringParts: Array<string> = [];
+      stringParts.push(entityOrOperation.queryId);
+      stringParts.push('(');
+
+      const entries = Object.entries(entityOrOperation.arguments);
+      const len = entries.length;
+
+      Object.entries(entityOrOperation.arguments).forEach(
+        ([key, value], idx) => {
+          stringParts.push(key);
+          stringParts.push(': ');
+          stringParts.push(JSON.stringify(value));
+          if (idx < len - 1) {
+            stringParts.push(', ');
+          }
+        }
+      );
+
+      stringParts.push(')');
+
+      return `${entityOrOperation.type}:${stringParts.join('')}`;
+    }
+
+    const entity = entityOrOperation;
+
+    return entity.id;
+
+    // if (isEntity(entity)) {
+    //   let key = this.getKey(entity);
+
+    //   if (key) {
+    //     return `${entity.__typename}:${key}`;
+    //   } else {
+    //     if (!entity) {
+    //       throw new Error(
+    //         '@data-eden/athena: Unable to derive key for entity which is undefined'
+    //       );
+    //     } else {
+    //       key = `${entity.__typename}:${btoa(
+    //         JSON.stringify({
+    //           prop: entityOrOperation.prop,
+    //           parentId:
+    //             entityOrOperation.parent &&
+    //             this.computeCacheKey({ entity: entityOrOperation.parent }),
+    //           entityType: entity.__typename,
+    //         })
+    //       )}`;
+    //     }
+    //   }
+
+    //   return key;
+    // } else {
+    //   throw new Error(
+    //     '@data-eden/athena: Unable to derive key for entity or operation'
+    //   );
+    // }
+  }
+
+  async processEntities<Data extends { [key: string]: any }>(
     response: Data,
-    operation?: GraphQLOperation<Data>
+    operation?: Operation<Data>
   ): Promise<Data> {
     const parsedEntitiesList = parseEntities(response);
 
@@ -205,51 +401,50 @@ export class SignalCache {
      *   [ car ]
      * ]
      */
-    const tx = await this.cache.beginTransaction({
-      fetchMore: !!operation?.fetchMore,
-    });
+    // const tx = await this.cache.beginTransaction({
+    //   fetchMore: !!operation?.fetchMore,
+    // });
 
-    for (const parsedEntities of parsedEntitiesList) {
-      for (const { parent, prop, entity } of parsedEntities) {
-        let key = this.getCacheKey(entity);
+    // for (const parsedEntities of parsedEntitiesList) {
+    //   for (const { parent, prop, entity } of parsedEntities) {
+    //     let key = this.getCacheKey(entity);
 
-        if (!key) {
-          // set a synthetic shallow cache key
-          key = this.getSyntheticKey(
-            { parent, prop, entity },
-            this.getCacheKey
-          );
-        }
+    //     if (!key) {
+    //       // set a synthetic shallow cache key
+    //       key = this.getSyntheticKey(
+    //         { parent, prop, entity },
+    //         this.getCacheKey
+    //       );
+    //     }
 
-        // replace the entity object with the key we're using to store it in the cache so that we can
-        // later replace the key with the reactive entity
-        // e.g. { pet: { id: 1, name: hitch }} -> { pet: 'pet:1' }
-        if (parent && prop) {
-          set(parent, prop, createLinkNode(key));
-        }
+    //     // replace the entity object with the key we're using to store it in the cache so that we can
+    //     // later replace the key with the reactive entity
+    //     // e.g. { pet: { id: 1, name: hitch }} -> { pet: 'pet:1' }
+    //     if (parent && prop) {
+    //       set(parent, prop, createLinkNode(key));
+    //     }
 
-        if (!parent) {
-          roots.set(prop, key);
-        }
+    //     if (!parent) {
+    //       roots.set(prop, key);
+    //     }
 
-        await tx.merge(key, entity);
-      }
-    }
-    await tx.commit();
+    //     await tx.merge(key, entity);
+    //   }
+    // }
+    // await tx.commit();
 
-    const result = {} as Data;
+    // const result = {} as Data;
 
-    for (const [propertyPath, cacheKey] of roots.entries()) {
-      set(result, propertyPath, this.signalCache.resolve(cacheKey));
-    }
+    // for (const [propertyPath, cacheKey] of roots.entries()) {
+    //   set(result, propertyPath, this.signalCache.resolve(cacheKey));
+    // }
 
-    if (operation) {
-      this.signalCache.storeOperation(operation, roots);
-    }
+    // if (operation) {
+    //   this.signalCache.storeOperation(operation, roots);
+    // }
 
     return result;
   }
-}
 
   storeEntity() {}
   resolve(op: Operation | Entity) {}
